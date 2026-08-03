49c57d3c1fbc0c4727cdfa87b44fce533088b586
Your Name <vishalsinha.inbox@gmail.com>
2026-08-04 02:09:18 +0530

    initial commit

---
+++ src/main/java/com/backend/geosentinel/GeosentinelApplication.java
@@ -0,0 +1,13 @@
+package com.backend.geosentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeosentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeosentinelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/geosentinel/GeosentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.geosentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeosentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
